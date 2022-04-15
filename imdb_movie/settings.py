6ef7fca4a453a1a3dc56835b7d1ecd69efba4ee2
imdadhussain <imdadhussain530@gmail.com>
2022-04-15 18:56:59 +0530

    change allow host

--- imdb_movie/settings.py
+++ imdb_movie/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2^3(lt5g9$2!z2&ax%g0j68&xf8!37crp_*@gca2hinxcg(n)8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*', 'imdbmovie-api.herokuapp.com']
+ALLOWED_HOSTS = ['*', 'https://imdbmovie-api.herokuapp.com']
 
 
 # Application definition
